d63777b85925ab4421107a01fcda9e5efe68af3a
Shetul Chothani <shetul-chothani@idexx.com>
2016-08-22 16:35:42 -0400

    no message

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fakeArray = require('./modules/fakeArray.js');
 var app = express();
+var PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   //res.send('Hello World once more!');
   res.render('index.ejs', {
