0984de4410ea738b87513622b4e8f803fe7d66ee
Shetul Chothani <shetul-chothani@idexx.com>
2016-08-22 15:52:40 -0400

    First node project

---
+++ modules/fakeArray.js
@@ -0,0 +1,1 @@
+module.exports = [1, 'string', 234];
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var fakeArray = require('./modules/fakeArray.js');
 var app = express();
+app.get('/', function (req, res) {
+  //res.send('Hello World once more!');
+  res.render('index.ejs', {
+    dataArray:fakeArray
+  });
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+console.log(fakeArray);
